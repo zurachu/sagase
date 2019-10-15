5366f8d6154c36fa30c8f9f85d0ca2f6534f4805
zurachu <zurachu@gmail.com>
2019-10-16 08:55:12 +0900

    都道府県データをScriptableObjectとして作成

---
+++ Assets/Scripts/Prefectures.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptableObject/Create Prefectures")]
+public class Prefectures : ScriptableObject
+{
+    [Serializable]
+    public struct Prefecture
+    {
+        public string name;
+        public Sprite sprite;
+    }
+
+    public int Count => prefectures.Count;
+
+    public Prefecture Get(int index) => prefectures[index];
+
+    public List<string> Names => prefectures.ConvertAll(_pref => _pref.name);
+
+    [SerializeField] private List<Prefecture> prefectures;
+}
