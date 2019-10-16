4caf12369b50f5d810bec1914df1097301af9b33
zurachu <zurachu@gmail.com>
2019-10-17 00:41:36 +0900

    タップできる都道府県のアイテムと必要なフォント、シェーダ

---
+++ Assets/Scripts/PrefectureItem.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PrefectureItem : MonoBehaviour
+{
+    [SerializeField] Image image;
+    [SerializeField] GameObject right;
+    [SerializeField] GameObject miss;
+    [SerializeField] Text nameText;
+
+    private bool isRight;
+
+    public void Initialize(Sprite sprite, string name, bool isRight)
+    {
+        this.isRight = isRight;
+
+        image.sprite = sprite;
+        nameText.text = name;
+        ClearStatus();
+    }
+
+    public void OnTap()
+    {
+        if (right.activeSelf || miss.activeSelf)
+        {
+            return;
+        }
+
+        if (isRight)
+        {
+            right.SetActive(true);
+        }
+        else
+        {
+            miss.SetActive(true);
+        }
+
+        nameText.gameObject.SetActive(true);
+    }
+
+    public void ClearStatus()
+    {
+        right.SetActive(false);
+        miss.SetActive(false);
+        nameText.gameObject.SetActive(false);
+    }
+}
