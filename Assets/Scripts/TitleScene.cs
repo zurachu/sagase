84d1d1e2452536509b2a59e6c4d78e0ae09e0e15
zurachu <zurachu@gmail.com>
2019-10-19 11:57:09 +0900

    Tap SAGA to START調整

--- Assets/Scripts/TitleScene.cs
+++ Assets/Scripts/TitleScene.cs
@@ -9,12 +9,15 @@ public class TitleScene : MonoBehaviour
     [SerializeField] private Canvas canvas;
     [SerializeField] List<Image> prefectures;
     [SerializeField] Image saga;
+    [SerializeField] CanvasGroup tapStart;
 
+    private Tweener tapStartTweeener;
     private Tweener sagaColorTweener;
 
     private void Start()
     {
         sagaColorTweener = saga.DOColor(new Color(0.5f, 1f, 0.5f), 0.5f).From(new Color(0f, 0.875f, 0f)).SetLoops(-1, LoopType.Yoyo);
+        tapStartTweeener = tapStart.DOFade(1f, 0.5f).From(0f).SetLoops(-1, LoopType.Yoyo);
     }
 
     public void OnTapStart()
@@ -26,7 +29,7 @@ public class TitleScene : MonoBehaviour
         {
             prefecture.transform.DOLocalMove(Vector3.zero, 0.5f);
             prefecture.transform.DOScale(Vector3.one, 0.5f);
-        }   
+        }
 
         sagaColorTweener.Kill();
         var sequence = DOTween.Sequence()
@@ -34,5 +37,8 @@ public class TitleScene : MonoBehaviour
             .AppendInterval(0.25f)
             .AppendCallback(() => { SceneManager.LoadScene("InGameScene"); })
             .Play();
+
+        tapStartTweeener.Kill();
+        tapStartTweeener = tapStart.DOFade(1f, 0.1f).From(0f).SetLoops(-1, LoopType.Yoyo);
     }
 }
